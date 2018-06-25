8da096928a814db6b678bb6d1211aa507e7dfd59
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-06-25 12:31:05 -0700

    fix CRUD Test

--- test/test-server.js
+++ test/test-server.js
@@ -1,13 +1,13 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-const { app, runServer, closeServer } = require("../server");
+const {app, runServer, closeServer} = require('../server');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe("Blog Posts", function() {
+describe('Blog Posts', function() {
     before(function() {
         return runServer();
     });
@@ -16,62 +16,75 @@ describe("Blog Posts", function() {
         return closeServer();
     });
 
-    it("should list items on GET", function() {
-        return chai
-        .request(app)
-        .get("/blog-posts")
+    it('should list items on GET', function() {
+        return chai.request(app)
+        .get('/blog-posts')
         .then(function(res) {
             expect(res).to.have.status(200);
             expect(res).to.be.json;
+            expect(res.body).to.be.a('array');
+            expect(res.body.length).to.be.above(0);
+            res.body.forEach(function(item) {
+                expect(item).to.be.a('object');
+                expect(item).to.have.all.keys(
+                    'id', 'title', 'content', 'author', 'publishDate');
+            });
         });
     });
 
-    it("should add an item on POST", function() {
-        const newItem = { title: "Shonen in 2010s", content: "Checkout Dragonball Super, My Hero Academia, and One Punch Man", author: "Proud Saiyan"};
-        return chai
-            .request(app)
-            .post("/blog-posts")
-            .send(newItem)
+    it('should add a blog post on POST', function() {
+        const newPost = { title: "Shonen in 2010s", content: "Checkout Dragonball Super, My Hero Academia, and One Punch Man", author: "Proud Saiyan"};
+        const expectedKeys = ['id', 'publishDate'].concat(Object.keys(newPost));
+        return chai.request(app)
+            .post('/blog-posts')
+            .send(newPost)
             .then(function(res) {
                 expect(res).to.have.status(201);
                 expect(res).to.be.json;
-                expect(res.body).to.be.a("object");
-                expect(res.body).to.include.keys("id", "title", "content", "author");
-                expect(res.body.id).to.not.equal(null);
-                expect(res.body).to.deep.equal(
-                    Object.assign(newItem, {id: res.body.id })
-                );
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.all.keys(expectedKeys);
+                expect(res.body.title).to.equal(newPost.title);
+                expect(res.body.content).to.equal(newPost.content);
+                expect(res.body.author).to.equal(newPost.author);
             });
     });
 
-    it("should add an item on PUT", function() {
-        const updateData = { title: "Nostalgia in Shonene", content: "Fist of the North Star, Yu-Yu Hakisho, Saint Seiya, Dragon Ball Z", author: "Classic Anime" };
-        return chai
-            .request(app)
-            .get("/blog-posts")
-            .send(newItem)
+    it('should error if POST missing expected values', function() {
+        const badRequestData = {};
+        return chai.request(app)
+            .post('/blog-posts')
+            .send(badRequestData)
             .then(function(res) {
-                updateData.id = res.body[0].id;
-                return chai
-                .request(app)
-                .put(`/blog-posts/${updateData.id}`)
-                .send(updateData);
-            })
-            .then (function(res) {
-                expect(res).to.have.status(200);
-                expect(res).to.be.json;
-                expect(res.body).to.be.a("object");
-                expect(res.body).to.deep.equal(updateData);
+                expect(res).to.have.status(400);
+            });
+    });
+
+    it('should update blog posts on PUT', function() {
+        return chai.request(app)
+            .get('/blog-posts')
+            .then(function(res) {
+                const updatedPost = Object.assign(res.body[0], {
+                    title: 'Nostalgia Shonen',
+                    content: 'Fist of the North Star, Saint Seiya, Dragon ball Z'
+                });
+                return chai.request(app)
+                .put(`/blog-posts/${res.body[0].id}`)
+                .send(updatedPost)
+                .then(function(res) {
+                    expect(res).to.have.status(204);
+                });
             });
     });
 
     it("should delete items on DELETE", function() {
-        return (
-            chai.request(app)
-            .get("/blog-posts")
+        return chai.request(app)
+            .get('/blog-posts')
             .then(function(res) {
-                expect(res).to.have.status(204)
-            })
-        );
-    });
+                return chai.request(app)
+                .delete(`/blog-posts/${res.body[0].id}`)
+                .then(function(res) {
+                    expect(res).to.have.status(204);
+                });
+            });
+        });
 });
